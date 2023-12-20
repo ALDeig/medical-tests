ccf37b11d1fcdbdfaf33e5a9a8b7da0706be397c
renovatio <alkin.denis@gmail.com>
2023-12-20 22:59:48 +0400

    feat: change DB from postges on mysql

--- app/src/services/db/base.py
+++ app/src/services/db/base.py
@@ -8,5 +8,5 @@ class Base(AsyncAttrs, DeclarativeBase):
     pass
 
 
-engine = create_async_engine(str(settings.postgres_dsn))
+engine = create_async_engine(str(settings.mysql_dsn))
 session_factory = async_sessionmaker(engine, expire_on_commit=False)
--- migrations/env.py
+++ migrations/env.py
@@ -13,7 +13,7 @@ from app.settings import settings
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("sqlalchemy.url", str(settings.postgres_dsn))
+config.set_main_option("sqlalchemy.url", str(settings.mysql_dsn))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
